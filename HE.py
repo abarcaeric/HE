6df16a23ecadfc1af11514c28d1fe5f0903fa6f9
Eric Abarca Calfuqueo <ericabarcacalfuqueo@MacBook-Pro-de-Eric.local>
2025-02-04 16:00:36 -0300

    Se agrega archivo requirements para las librerias

--- HE.py
+++ HE.py
@@ -11,6 +11,8 @@ chat_id = "@hechosesencialeschile"
 timer = 20
 fecha_old = ''
 
+#print ("hola1")
+
 while True:
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     url = "https://www.cmfchile.cl/institucional/hechos/hechos_portada.php"
